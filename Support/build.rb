9c39e7b0fccaaecb27e52d64bdf4141528ab340e
Myles Byrne <myles@myles.id.au>
2011-01-24 17:32:56 +1100

    everything should be called BundleMate now, simpler language grammar

--- Support/build.rb
+++ Support/build.rb
@@ -6,6 +6,9 @@ bundle_dir  = File.dirname(source_file)
 targets = YAML.load(File.read(source_file))
 
 targets.each do |name, data|
+  file = bundle_dir + '/' + name
+  dir = File.dirname file
+  Dir.mkdir dir unless File.exists? dir
   File.open(bundle_dir + '/' + name, 'w') do |f|
     f.write compile_tm_plist(data,name)
   end
